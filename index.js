a2dde394e8927fdbb9ec4c4cdfdfc89f9db50afd
Pragadeeswaran <pragadees24@gmail.com>
2020-05-31 14:39:15 +0530

    Enabling DB.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); 
+const cors = require('cors');
 
-//const { mongoose } = require('./db.js'); //destructuring syntax from ES6
+const { mongoose } = require('./db.js'); //destructuring syntax from ES6
 var employeeController = require('./controllers/employeeConroller.js');
 var app = express();
 
 app.use(bodyParser.json());
-app.use(cors({origin: 'http://localhost:4200'}));
-app.listen(3000,()=>console.log('Express is up and Running'));
+app.use(cors({ origin: 'http://localhost:4200' }));
+app.listen(3000, () => console.log('Express is up and Running'));
 
-app.use('/employees',employeeController);
+app.use('/employees', employeeController);
